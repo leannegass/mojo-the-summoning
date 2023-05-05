1f46cfd514b6ef1add936b583b3ebdc4584ddac4
Leanne Gass <leannegass@gmail.com>
2023-05-05 11:35:00 +0100

    Revert "created models"
    This reverts commit 51cdaf761ee20afbc9a5820d27529d8ea2bbe2bf.

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,13 +1,1 @@
-const {Sequelize, sequelize, dataTypes, Model} = require('../db/config.js');
 // create your User model here
-
-let User = sequelize.define("User", {
-    //create user properties
-    name : DataTypes.STRING,
-    email : DataTypes.STRING
-
-});
-
-module.exports = {
-    User
-};
